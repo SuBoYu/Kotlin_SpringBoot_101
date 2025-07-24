cb2474ca58d49459c3c4dbbc9fcef9ba9c7a8883
Tony Su <boyu.su@newsbreak.com>
2025-07-24 16:24:27 -0700

    Finish 101

--- src/main/kotlin/com/example/demo/MessageService.kt
+++ src/main/kotlin/com/example/demo/MessageService.kt
@@ -1,25 +1,37 @@
 package com.example.demo
 
+//import org.springframework.stereotype.Service
+//import org.springframework.jdbc.core.JdbcTemplate
+//import org.springframework.jdbc.core.query
+//import java.util.UUID
+//
+//@Service
+//class MessageService(private val db: JdbcTemplate) {
+//    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+//        Message(response.getString("id"), response.getString("text"))
+//    }
+//
+//    fun findMessageById(id: String): Message? = db.query("select * from messages where id = ?", id)
+//    {response, _ -> Message(response.getString("id"), response.getString("text"))
+//    }.singleOrNull()
+//
+//    fun save(message: Message): Message {
+//        val id = message.id ?: UUID.randomUUID().toString()
+//        db.update("insert into messages values ( ?, ? )",
+//            id, message.text
+//        )
+//        return message.copy(id = id)
+//    }
+//}
+
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.jdbc.core.query
-import java.util.UUID
 
 @Service
-class MessageService(private val db: JdbcTemplate) {
-    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
-        Message(response.getString("id"), response.getString("text"))
-    }
+class MessageService(private val db: MessageRepository) {
+    fun findMessages(): List<Message> = db.findAll().toList()
 
-    fun findMessageById(id: String): Message? = db.query("select * from messages where id = ?", id)
-    {response, _ -> Message(response.getString("id"), response.getString("text"))
-    }.singleOrNull()
+    fun findMessageById(id: String): Message? = db.findByIdOrNull(id)
 
-    fun save(message: Message): Message {
-        val id = message.id ?: UUID.randomUUID().toString()
-        db.update("insert into messages values ( ?, ? )",
-            id, message.text
-        )
-        return message.copy(id = id)
-    }
+    fun save(message: Message): Message = db.save(message)
 }
